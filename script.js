e2d0d36ff11a299b8bee3a57aa474501af51205c
Felipe Galdino <alunofelipegaldino9ano@gmail.com>
2024-02-07 23:19:17 -0300

    completo

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ const products = [
   },
 ];
 
+const productsList = document.querySelector('ul')
+
+for (let i = 0; i < products.length; i++) {
+   const listItem = document.createElement('li')
+
+   const productName = document.createElement('h3')
+   const productPrice = document.createElement('p')
+   const addToCartButton = document.createElement('button')
+
+   productName.innerText = products[i].name
+   productPrice.innerText = products[i].price
+   addToCartButton.innerText = "Adicionar ao carrinho"
+
+   listItem.appendChild(productName)
+   listItem.appendChild(productPrice)
+   listItem.appendChild(addToCartButton)
+
+   productsList.appendChild(listItem)
+}
